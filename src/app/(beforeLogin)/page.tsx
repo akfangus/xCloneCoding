01b6168a665fa69faf36861e2411b9d5943f2971
akfangus <akfanakfan@naver.com>
2024-01-18 21:17:57 +0900

    병렬 라우팅 + 라우팅 인터셉트

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import zlogo from '../../public/xlogo.png';
+import zlogo from '/public/xlogo.png';
 
-export default function Home() {
+export default function Main() {
     return (
-        <main>
+        <main className="flex items-center justify-center w-full h-screen gap-8">
             <div>
                 <Image src={zlogo} alt="logo" width={300} height={300} />
             </div>
@@ -13,7 +13,7 @@ export default function Home() {
                 <h2>지금 가입하세요.</h2>
                 <Link href="/i/flow/signup">계정 만들기</Link>
                 <h3>이미 트위터에 가입 하셨나요?</h3>
-                <Link href="/login">로그인</Link>
+                <Link href="/i/flow/login">로그인</Link>
             </div>
         </main>
     );
